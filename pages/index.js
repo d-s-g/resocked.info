0f38b53518b6de08df0a1228293dea855c776fa8
David Gibson <david.stanford.gibson@gmail.com>
2020-04-27 13:24:28 -0400

    mapbox ziplook up

--- pages/index.js
+++ pages/index.js
@@ -1,188 +1,77 @@
 import Head from "next/head";
-import { useEffect } from "react";
+import Link from "next/link";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 import { useUser } from "../context/userContext";
-import firebase from "../firebase/clientApp";
 import styled from "styled-components";
 
-const Red = styled.main.attrs({
-  className: `thisisnotused`
-})``;
+const mapboxGeoToken = {
+  mapboxApiAccessToken: process.env.MAPBOX_API_ACCESS_TOKEN
+};
 
-export default () => {
-  // Our custom hook to get context values
-  const { loadingUser, user } = useUser();
+const Index = props => {
+  const [data, setData] = useState([]);
+  const [inputValue, setValue] = useState({});
 
-  useEffect(() => {
-    if (!loadingUser) {
-      // You know that the user is loaded: either logged in or out!
-      console.log(user);
-    }
-    // You also have your firebase app initialized
-    console.log(firebase);
-  }, [loadingUser, user]);
+  async function fetchData(query) {
+    const result = await fetch(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/groceries%20${query}.json?country=us&limit=10&access_token=${mapboxGeoToken.mapboxApiAccessToken}`
+    );
+
+    const json = await result.json();
+
+    await setData(json.features.map(store => store));
+  }
+
+  const handleChange = e => {
+    const { name, type, value } = e.target;
+    const inputValue = type === "number" ? parseFloat(value) : value;
+    setValue({ [name]: inputValue });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    //send request to mapbox
+    fetchData(inputValue.zip);
+  };
 
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js w/ Firebase Client-Side</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Red>
-        <h1 className="title">Next.js w/ Firebase Client-Side</h1>
-        <p className="description">Fill in your credentials to get started</p>
-      </Red>
-
-      <footer>
-        <a
-          href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input
+          type="number"
+          name="zip"
+          onChange={handleChange}
+          required
+          placeholder="Enter a zip"
+        />
+      </label>
+      <input type="submit" value="Submit" />
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>
+            <Link
+              href={{
+                pathname: "/Location",
+                query: {
+                  id: item.id.replace("poi.", ""),
+                  title: item.text,
+                  address: item.properties.address
+                },
+                shallow: true
+              }}
+            >
+              <a>{item.place_name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </form>
   );
 };
+
+Index.propTypes = {
+  inputValue: PropTypes.number
+};
+
+export default Index;
